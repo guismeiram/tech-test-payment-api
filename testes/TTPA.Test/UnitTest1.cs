8d858012c2ec64b48a9dfb084befe7bd15640581
guismeiram <boralagui@outlook.com>
2022-12-14 22:24:30 -0300

    primeiro commit do NUnit

---
+++ testes/TTPA.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TTPA.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
